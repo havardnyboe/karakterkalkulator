ea2d7549e669d5316908b321fabe56d2570f4485
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2022-10-30 17:50:22 +0100

    initial react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1>Hello, World!</h1>
+      <p>👋</p>
+      <code>Work in progress...</code>
+    </div>
+  );
+}
+
+export default App;
